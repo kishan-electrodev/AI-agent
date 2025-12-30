8aa68490afcf52f84632218e4bf3f87c4e8a3d48
Kishan Kumar <121043195+kishan-electrodev@users.noreply.github.com>
2025-12-30 16:31:45 +0000

    added login

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,27 +1,46 @@
 const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+const backendURL = import.meta.env.VITE_BACKEND_URL;
 import { Clerk } from '@clerk/clerk-js'
 
 const clerk = new Clerk(clerkPubKey)
 await clerk.load()
 
 
-if (clerk.isSignedIn) {
-  document.querySelector('.login-btn').innerHTML = `
-    <div id="user-button"></div>
-  `
+// show login button if user not logged in else show user button
 
-  const userButtonDiv = document.getElementById('user-button')
-
-  clerk.mountUserButton(userButtonDiv)
+if (!clerk.user) {
+  document.querySelector('.login-btn').onclick = () => clerk.openSignIn()
+  
 } else {
-  document.getElementById('app').innerHTML = `
-    <div id="sign-in"></div>
+
+  //save user to backend
+  saveUserToBackend(clerk.user);
+
+  document.querySelector('.login-btn').innerHTML = 
   `
+  <div class="user-area">
+    <div id=user-button></div>
+  </div>`
 
-  const signInDiv = document.getElementById('sign-in')
+  //Now mount user icon
+  const userBtn = document.getElementById('user-button');
+  clerk.mountUserButton(userBtn);
 
-  clerk.mountSignIn(signInDiv,{
-  afterSignInUrl: '/index.html',
-  afterSignUpUrl: '/index.html',
-})
+  document.querySelector('.greeting h1').innerHTML = 
+  `<span class="status-dot"></span>Hi ${clerk.user.fullName}`
+}
+
+// save function
+async function saveUserToBackend(user){
+  await fetch(`${backendURL}/users`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      id: user.id,
+      email: user.primaryEmailAddress.emailAddress,
+      name: user.fullName
+    })
+  });
 }
