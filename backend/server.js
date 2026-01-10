e7a3abb3343fd02adf610a6b3d7d7e440d68d1a5
Kishan Kumar <121043195+kishan-electrodev@users.noreply.github.com>
2026-01-10 16:26:02 +0000

    grok api ai added in backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import {neon} from '@neondatabase/serverless';
+import { neon } from '@neondatabase/serverless';
 
 dotenv.config();
 
@@ -11,16 +11,17 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-const sql = neon(process.env.DATABASE_URL);
+// const sql = neon(process.env.DATABASE_URL);
 
-// Create table if not exists
-async function createTable(){
-    await sql`CREATE TABLE IF NOT EXISTS users (id TEXT PRIMARY KEY, email TEXT, creatd_at TIMESTAMP DEFAULT NOW())`;
-    await sql`CREATE TABLE IF NOT EXISTS chats (id SERIAL PRIMARY KEY, user_id TEXT REFERENCES users(id), title TEXT, creatd_at TIMESTAMP DEFAULT NOW())`;
-    await sql`CREATE TABLE IF NOT EXISTS messages (id SERIAL PRIMARY KEY, chat_id INT REFERENCES chats(id), user_id TEXT, role TEXT, text TEXT, creatd_at TIMESTAMP DEFAULT NOW())`;
-}
+// // Create table if not exists
+// async function createTable(){
+//     await sql`CREATE TABLE IF NOT EXISTS users (id TEXT PRIMARY KEY, email TEXT, creatd_at TIMESTAMP DEFAULT NOW())`;
+//     await sql`CREATE TABLE IF NOT EXISTS chats (id SERIAL PRIMARY KEY, user_id TEXT REFERENCES users(id), title TEXT, creatd_at TIMESTAMP DEFAULT NOW())`;
+//     await sql`CREATE TABLE IF NOT EXISTS messages (id SERIAL PRIMARY KEY, chat_id INT REFERENCES chats(id), user_id TEXT, role TEXT, text TEXT, creatd_at TIMESTAMP DEFAULT NOW())`;
+// }
 
-createTable();
+// createTable();
+
 
 
 app.listen(port, () => {
